67f8f728a3f9015102d48b5b3b3650b25fd220ed
gilovan <realgrishailovan@gmail.com>
2024-11-24 15:26:39 +0300

    fix bags

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import os
 import sys
 
 import requests
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
 import asyncio
 
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.types import InputMediaPhoto, FSInputFile
+from aiogram.exceptions import TelegramRetryAfter
 
 from src.conf import redis
 
@@ -74,32 +75,41 @@ def download_media_files(base_url: str, channel: str, id_post: int) -> tuple[str
 
 
 async def send_message():
-    message = redis.receive_from_queue()
-    if message and "content" in message and isinstance(message["content"], str):
-        print(message["content"])
-        path_media, media = download_media_files(base_url="http://0.0.0.0:8000/news", channel=message["channel"], id_post=message["id_post"])
-        list_media = []
-        content = message["content"]
+    try:
+        message = redis.receive_from_queue()
+        if message and "content" in message and isinstance(message["content"], str):
+            print(message["content"])
+            path_media, media = download_media_files(base_url="http://0.0.0.0:8000/news", channel=message["channel"], id_post=message["id_post"])
+            list_media = []
+            content = message["content"]
 
-        if path_media:
-            for files in media:
-                if len(list_media) < 10:
-                    if not list_media:
-                        preview_text = content[:1024] + ("..." if len(content) > 1024 else "")
-                        list_media.append(InputMediaPhoto(media=FSInputFile(path=os.path.join(files)), caption=preview_text))
-                    else:
-                        list_media.append(InputMediaPhoto(media=FSInputFile(path=os.path.join(files))))
-            
-            if list_media:
-                await bot.send_media_group(chat_id=PUBLIC_CHAT_ID, media=list_media)
-                if len(content) > 1024:
+            if path_media:
+                for files in media:
+                    if len(list_media) < 10:
+                        if not list_media:
+                            preview_text = content[:900] + ("..." if len(content) > 900 else "")
+                            list_media.append(InputMediaPhoto(media=FSInputFile(path=os.path.join(files)), caption=preview_text))
+                        else:
+                            list_media.append(InputMediaPhoto(media=FSInputFile(path=os.path.join(files))))
+                
+                if list_media:
+                    try:
+                        await bot.send_media_group(chat_id=PUBLIC_CHAT_ID, media=list_media)
+                        await asyncio.sleep(5)
+                        if len(content) > 900:
+                            await bot.send_message(chat_id=PUBLIC_CHAT_ID, text=content)
+                    except TelegramRetryAfter as e:
+                        await asyncio.sleep(e.retry_after)
+                        await bot.send_media_group(chat_id=PUBLIC_CHAT_ID, media=list_media)
+                else:
                     await bot.send_message(chat_id=PUBLIC_CHAT_ID, text=content)
             else:
                 await bot.send_message(chat_id=PUBLIC_CHAT_ID, text=content)
         else:
-            await bot.send_message(chat_id=PUBLIC_CHAT_ID, text=content)
-    else:
-        print("Ошибка: Нет контента или неверный формат данных.")
+            print("Ошибка: Нет контента или неверный формат данных.")
+    except Exception as e:
+        print(f"Произошла ошибка: {str(e)}")
+    
     await asyncio.sleep(1)
 
 async def main() -> None:
@@ -109,3 +119,4 @@ async def main() -> None:
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
+
